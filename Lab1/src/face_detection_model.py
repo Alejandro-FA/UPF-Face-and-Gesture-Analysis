0decfa68eabaf58b4e33806dd1ee44e2de7f4ef4
AndreuG01 <andreu.garcies01@estudiant.upf.edu>
2024-01-14 16:52:15 +0100

    Rotations now use new framework. Removed old code and functions

--- Lab1/src/face_detection_model.py
+++ Lab1/src/face_detection_model.py
@@ -29,6 +29,7 @@ class FaceDetectionModel:
         self.eye_pair = eye_pair_model
 
         self.overlap_filter = OverlapFilter(threshold=OVERLAP_THRESHOLD)
+        self.img_count = 0
 
     def detect_faces(self, image) -> list[tuple[int, int, int, int]]:
         frame_gray = self.preprocess(image)
@@ -44,6 +45,7 @@ class FaceDetectionModel:
             eyes = self.detect_elements(self.eyes_cascade, eyesROI)
             # Only add them if it is also possible to find at least 1 eye
             if len(eyes) > 0:
+                # print(f"{self.img_count}: Checkpoint 1. profile and eyes detected")
                 detected_profiles.append(box)
         
         # ---------------------------------------------------------------------
@@ -56,6 +58,7 @@ class FaceDetectionModel:
             # Add face if it is found both with a lbpcascade and a haarcascade
             if len(haar_faces) > 0:
                 largest_face = self.__get_largest_faces(haar_faces, 1)[0]
+                # print(f"{self.img_count}: Checkpoint 2. lbp cascade and haar face detected")
                 detected_faces.append(largest_face)
 
             # If not, try to find other human elements
@@ -69,6 +72,7 @@ class FaceDetectionModel:
                 len(self.detect_elements(self.upper_body, bodyROI, scaleFactor=1.05)) > 0  or \
                 len(self.detect_elements(self.nose_cascade, smileROI, scaleFactor=1.05)) > 0:
                     adjusted_box = box.get_resized(base_image.width(), base_image.height(), margin=0.25)
+                    # print(f"{self.img_count}: Checkpoint 3. lbp cascade and some other elements")
                     detected_faces.append(adjusted_box)
 
         
@@ -83,24 +87,21 @@ class FaceDetectionModel:
             rotation_angles = [10, 15, 20, 25, 30, -10, -15, -20, -25, -30]
             rotated_images = [(self.preprocess(self.__rotate_image(image, angle)), angle) for angle in rotation_angles]
             for frame, angle in rotated_images:
-                faces = self.face_haarcascade.detectMultiScale(frame_gray)
-                added = False
-                rotated_face = None
-                for (x, y, w, h) in faces:
-                    rotated_face = self.__get_box(x, y, w, h, frame, 0)
-                    added = True
-                if added:
-                    rotated_face_ROI = frame[
-                        rotated_face[1]:rotated_face[1] + rotated_face[3],
-                        rotated_face[0]:rotated_face[0] + rotated_face[2]
-                    ]
-                    lbp_faces = self.face_lbpcascade.detectMultiScale(rotated_face_ROI, scaleFactor=1.05)
-                    profile_rotated = self.profile_lbpcascade.detectMultiScale(rotated_face_ROI, scaleFactor=1.05)
-                    if len(lbp_faces) > 0 or \
-                    len(profile_rotated) > 0:
-                        results.append(rotated_face)
+                frame_gray = self.preprocess(frame)
+                base_image = ROI(frame_gray)
+                rotated_faces = self.detect_elements(self.face_haarcascade, base_image)
+                face_added = False
+                for curr_face in rotated_faces:
+                    rotated_face_ROI = ROI(frame_gray, curr_face)
+                    if len(self.detect_elements(self.face_lbpcascade, rotated_face_ROI, scaleFactor=1.05)) > 0 or \
+                    len(self.detect_elements(self.profile_lbpcascade, rotated_face_ROI, scaleFactor=1.05)) > 0:
+                        # print(f"{self.img_count}: Checkpoint 4. rotation added")
+                        results.append(curr_face.get_coords())
+                        face_added = True
+                if face_added:
                     break
-                    
+                                        
+        self.img_count += 1
         return results
 
 
@@ -134,22 +135,6 @@ class FaceDetectionModel:
         return largest_faces
     
 
-    def __get_box(self, x, y, w, h, image, margin: float) -> tuple[int, int, int, int]:
-        img_h = image.shape[0]
-        img_w = image.shape[1]
-        return (
-            self.__clamp(x - (0 + margin) * w, 0, img_w),
-            self.__clamp(y - (0 + margin) * h, 0, img_h),
-            self.__clamp(x + (1 + margin) * w, 0, img_w),
-            self.__clamp(y + (1 + margin) * h, 0, img_h)
-        )
-
-    def __clamp(self, x: float, min_threshold, max_threshold) -> int:
-        x = int(x)
-        x = min(x, max_threshold)
-        x = max(x, min_threshold)
-        return x
-    
     
     def __rotate_image(self, image, angle):    
         height, width = image.shape[:2]
